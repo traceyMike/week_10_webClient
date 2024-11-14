868d3abc1096527f0f564bfd7390a3dd9ce79c95
traceyMike <your-email@example.com>
2024-11-14 11:18:34 -0600

    initial

---
+++ lab-11-bmi-components/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import BodyMassIndexForm from './components/BodyMassIndexForm.vue';
+</script>
+
+<template>
+  <h1>Body Mass Index Calculator</h1>
+  <BodyMassIndexForm/>
+</template>
+
+<style scoped>
+
+</style> <!--script, template, file - three components of vue files-->
+
+<!-- need v model here-->
+<label>height</label><input></input>
+
+<!-- need v model-->
+<lable>weight</lable><input></input>
+
+<button>Calculator</button>
